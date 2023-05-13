e0608ff553f18ffb15a4543e04c2c02e1a52f4e9
derpsol <franklyjin029@gmail.com>
2023-05-12 21:20:05 -0700

    adding, approving nft address functions are completed

--- src/modules/approved/approved.routes.js
+++ src/modules/approved/approved.routes.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const approvedCtrl = require('./approved.controller');
+const express = require("express");
+const approvedCtrl = require("./approved.controller");
 
 const router = express.Router();
 
-router.route('/')
-  .get(approvedCtrl.list);
+router.route("/").get(approvedCtrl.list);
 
-router.route('/create')
-  .post(approvedCtrl.create)
+router.route("/create").post(approvedCtrl.create);
+
+router.route("/delete/:address").delete(approvedCtrl.remove);
 
 module.exports = router;
